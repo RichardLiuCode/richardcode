6b97a611930e5dd00c5dd40c27b78a9d8140c3df
Richard Liu <richardadesigner@gmail.com>
2025-06-28 02:41:51 -0700

    Update view source hint.js

--- view source hint.js
+++ view source hint.js
@@ -2,9 +2,10 @@ document.addEventListener("keydown", function (event) {
             if (event.key == "F12" || (event.shiftKey && event.ctrlKey && event.key == "I")) {
                 console.clear();
                 console.log("Hi! Are you trying to view source code, There are more way that you can view this website's source code!");
-                console.log("1️⃣ You can go to \n👉https://github.com/richardliucode/richardcode \nto view the source code of this website!");
-                console.log("2️⃣ You can click Ctrl + U to view source code");
-                console.log("3️⃣ If you stay, go to the Element tag at the top to view source code.")
+                console.log("1️⃣ Go to \n👉https://github.com/richardliucode/richardcode \nto view the source code of this website!");
+                console.log("2️⃣ Click Ctrl + U to view source code");
+                console.log("3️⃣ If you stay, go to the Element tag at the top to view source code.");
+                console.log("4️⃣ Add the word “view-source:” at the very begining of the address input that are at the top of the browser.");
                 console.log("🖥️ Website developed by Richard Liu");
             }
 })
