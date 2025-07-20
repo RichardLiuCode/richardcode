bb8a142339d8c66c6fb8c9eca37bb924b3052f75
Richard Liu <richardadesigner@gmail.com>
2025-07-19 19:23:13 -0700

    Update WakeUpReplit.js

--- WakeUpReplit.js
+++ WakeUpReplit.js
@@ -1,9 +1,9 @@
 window.onload = function () {
     fetch("https://573f616c-2f33-4a25-8dd3-eab903401ca9-00-15qkzj0ppjof8.spock.replit.dev/calc?q=6")
         .then(function (responde) {
-            console.log("Server is running");
+            console.log("✅ Replit Server for Factorial Calculator App is running");
         })
         .catch(function (error) {
-            throw new Error("Server might be in sleep mode or stopped running");
+            throw new Error("🚫 Replit Server for Factorial Calculator App might be in sleep mode or stopped running");
         })
 }
