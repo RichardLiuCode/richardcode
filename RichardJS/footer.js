9752c1cf4e6856384fab63c6e685ed44f6b2be14
Richard Liu <richardadesigner@gmail.com>
2025-09-14 00:44:38 -0700

    update

--- RichardJS/footer.js
+++ RichardJS/footer.js
@@ -1,5 +1,5 @@
 window.onload = function () {
     var FooterJSEmbed = document.createElement('script');
-    FooterJSEmbed.src = 'https://richardliucode.github.io/richardcode/RichardJS/footerCodeContent.js'
+    FooterJSEmbed.src = 'https://richardliucode.github.io/richardcode/RichardJS/FooterCodeContent.js'
     document.body.appendChild(FooterJSEmbed)
 }
