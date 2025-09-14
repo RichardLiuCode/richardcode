239c64be1ce66825e1e6dc6f4a1f7eda8e646b0f
Richard Liu <richardadesigner@gmail.com>
2025-09-14 00:37:23 -0700

    add footerwithoutonload.js

---
+++ RichardJS/footerWithoutOnload.js
@@ -0,0 +1,6 @@
+var footerJS = {
+    "element": document.createElement("footer")
+}
+footerJS.element.innerHTML = "<p style=\"margin-left: 15px; \">© " + new Date().getFullYear() + " Richard Liu</p><p>RichardCode</p><p style=\"margin-right: 18px; \">Host by GitHub</p>"
+footerJS.element.setAttribute("style", "position: fixed;background-color: aliceblue;bottom: 0;left: 0;width: 100vw;height: 35px;    font-family: arial;    display: flex;    justify-content: space-between;    align-items: center;    user-select: none;")
+document.body.appendChild(footerJS.element)
