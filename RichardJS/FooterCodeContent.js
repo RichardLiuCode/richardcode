b08f665cd1ae5be3ed87ea7215dd3523ced98dbc
Richard Liu <richardadesigner@gmail.com>
2025-09-14 02:15:36 -0700

    update

--- RichardJS/FooterCodeContent.js
+++ RichardJS/FooterCodeContent.js
@@ -1,15 +1,24 @@
 var footerJS = {
-    "element": document.createElement("footer"),
+    "footerElement": document.createElement("footer"),
     "getHostPlatform": function () {
         if (window.location.href.includes(".github.io/")) {
             return "GitHub"
         } else if (window.location.href.includes(".onrender.com")) {
             return "Render"
-        }else if (window.location.href.includes("https://codeprojects.org/projects")) {
+        } else if (window.location.href.includes("https://codeprojects.org/projects")) {
             return "code.org"
+        } else {
+            return ""
+        }
+    },
+    "hostByPlatformText": function () {
+        if (footerJS.getHostPlatform() != "") {
+            return "Host by "
+        } else {
+            return "HTML / CSS / JS"
         }
     }
 }
-footerJS.element.innerHTML = "<p style=\"margin-left: 15px; \">© " + new Date().getFullYear() + " Richard Liu</p><p>RichardCode</p><p style=\"margin-right: 18px; \">Host by "+footerJS.getHostPlatform()+"</p>"
-footerJS.element.setAttribute("style", "position: fixed;background-color: aliceblue;bottom: 0;left: 0;width: 100vw;height: 35px;    font-family: arial;    display: flex;    justify-content: space-between;    align-items: center;    user-select: none;")
-document.body.appendChild(footerJS.element)
+footerJS.footerElement.innerHTML = "<p style=\"margin-left: 15px; \">© " + new Date().getFullYear() + " Richard Liu</p><p>RichardCode</p><p style=\"margin-right: 18px; \">" + footerJS.hostByPlatformText() + footerJS.getHostPlatform() + "</p>"
+footerJS.footerElement.setAttribute("style", "position: fixed;background-color: aliceblue;bottom: 0;left: 0;width: 100vw;height: 35px;    font-family: arial;    display: flex;    justify-content: space-between;    align-items: center;    user-select: none;")
+document.body.appendChild(footerJS.footerElement)
