cbd1f930d15181e72c765293083eae240d7a347c
Richard Liu <richardadesigner@gmail.com>
2025-09-14 01:03:39 -0700

    update footer.js

--- RichardJS/FooterCodeContent.js
+++ RichardJS/FooterCodeContent.js
@@ -8,6 +8,6 @@ var footerJS = {
         }
     }
 }
-footerJS.element.innerHTML = "<p style=\"margin-left: 15px; \">© " + new Date().getFullYear() + " Richard Liu</p><p>RichardCode</p><p style=\"margin-right: 18px; \">Host by GitHub</p>"
+footerJS.element.innerHTML = "<p style=\"margin-left: 15px; \">© " + new Date().getFullYear() + " Richard Liu</p><p>RichardCode</p><p style=\"margin-right: 18px; \">Host by "+footerJS.getHostPlatform()+"</p>"
 footerJS.element.setAttribute("style", "position: fixed;background-color: aliceblue;bottom: 0;left: 0;width: 100vw;height: 35px;    font-family: arial;    display: flex;    justify-content: space-between;    align-items: center;    user-select: none;")
 document.body.appendChild(footerJS.element)
