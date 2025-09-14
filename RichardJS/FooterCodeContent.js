7a192a1d91dfbdd8e1dc38261a899ebb8bcf5062
Richard Liu <richardadesigner@gmail.com>
2025-09-14 10:22:20 -0700

    update

--- RichardJS/FooterCodeContent.js
+++ RichardJS/FooterCodeContent.js
@@ -48,7 +48,7 @@ footerJS.footerSecondElement.style = "font-family:arial;display:none;background-
 document.body.appendChild(footerJS.footerSecondElement);
 /* The inside Element Inside The footer*/
 footerJS.footerIn234.style = "width:auto;height:50px;background-color:aliceblue;position:fixed;left:0;bottom:50px;"
-footerJS.footerIn234.innerHTML = "<ol><li>Richard's personal Wesite: <a href=\"https://richardliucode.github.io/richardcode\" style=\"text-decoration:none\">https://richardliucode.github.io/richardcode</a>.</li><li> Richard's Youtube Channel: <a href=\"https://www.youtube.com/channel/UCuvJA2I2rsiTfEPHqllr_dQ\" style=\"text-decoration:none\">https://www.youtube.com/channel/UCuvJA2I2rsiTfEPHqllr_dQ</a></li></ol>"
+footerJS.footerIn234.innerHTML = "<ol><li>Richard's personal Wesite: <a href=\"https://richardliucode.github.io/richardcode\" style=\"text-decoration:none\" target=\"_blank\">https://richardliucode.github.io/richardcode</a>.</li><li> Richard's Youtube Channel: <a href=\"https://www.youtube.com/channel/UCuvJA2I2rsiTfEPHqllr_dQ\" style=\"text-decoration:none\"  target=\"_blank\">https://www.youtube.com/channel/UCuvJA2I2rsiTfEPHqllr_dQ</a></li></ol>"
 footerJS.footerSecondElement.appendChild(footerJS.footerIn234)
 /* The top Left element inside the footer*/
 footerJS.footerIn6783.style = "width:660px;height:80px;background-color:aliceblue;position:fixed;left:20px;bottom:105px;";
