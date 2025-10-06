59814b8c0a6a1517474f3855dd3645815f9c5c75
Richard Liu <richardadesigner@gmail.com>
2025-10-05 18:37:28 -0700

    update add renderpaage

--- browserAndApplabCodePage.js
+++ browserAndApplabCodePage.js
@@ -3,12 +3,11 @@ document.getElementById("copyScriptURL").addEventListener("click", function () {
   navigator.clipboard.writeText(CopyScriptUrl);
 
   document.getElementById("scriptURLDisplay").select();
- console.log(navigator.clipboard.readText)
+  console.log(navigator.clipboard.readText)
 });
 document.getElementById("button5").addEventListener("click", function () {
-  var CopyScriptUrl = "https://richardcode.glitch.me/applab.js";
   navigator.clipboard.writeText("<script src='https://RichardLiuCode.github.io/richardcode/applab.js'></script>");
 
   document.getElementById("scriptTagDisplay").select();
- console.log(navigator.clipboard.readText)
+  console.log(navigator.clipboard.readText);
 });
