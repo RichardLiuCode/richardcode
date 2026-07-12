6b134cb8709787f096a2b11ea5e6ff0c612c69ec
Richard Liu <213156676+RichardLiuCode@users.noreply.github.com>
2026-07-11 21:24:51 -0700

    Remove obsolete files and code related to App Lab project, including JavaScript, HTML, CSS, and associated assets. This cleanup enhances project maintainability and reduces clutter.

--- richardcode-2025-05-23_234955/app/copyEmail.js
+++
@@ -1,4 +0,0 @@
-document.getElementById("copyEmail").addEventListener("click",function(){
-  console.log("Copy email button clicked");
-  navigator.clipboard.writeText("ray.chen@highschoolofthearts.org")
-})
